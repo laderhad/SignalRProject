6f44ce6063c9841503db41420727918013b72471
Utku Kerem <68512069+laderhad@users.noreply.github.com>
2024-04-16 01:17:27 +0300

    SignalR Kütüphanesi kuruldu ve gerekli konfigürasyonlar yapıldı

--- SignalRWebUI/Program.cs
+++ SignalRWebUI/Program.cs
@@ -1,9 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddHttpClient();
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
